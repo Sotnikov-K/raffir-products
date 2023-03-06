01240dc092dc55fcac3b3e56ab197a3898f3eb26
Kirill Sotnikov <sotnikovmail@yandex.ru>
2023-03-06 23:40:50 +1000

    blades page - frontend - blades section 3

--- resources/views/pages/blades-page/includes/blades-2-blades.blade.php
+++ resources/views/pages/blades-page/includes/blades-2-blades.blade.php
@@ -64,7 +64,7 @@
                 <div class="blades__item-wrapper blades__item-wrapper--blade">
                     <div class="blades__item">
                         <div class="blades__picture-wrapper">
-                            <img class="blades__picture" src="images/product_intro/blade/page-content/1.jpg" alt="Классический скандинавский клинок - Модель: Миккельсен ">
+                            <img class="blades__picture" src="images/product_intro/blade/page-content/2.jpg" alt="Классический скандинавский клинок - Модель: Миккельсен ">
                         </div>
                         <div class="blades__description-wrapper">
                             <p class="blades__description">Классический скандинавский клинок</p>
@@ -88,77 +88,107 @@
         </div>
 
 
-
-
-
-
-     
-
-        <div class="texture__data">
-            <div class="texture__data-left">
-                <div class="texture__item-wrapper texture__item-wrapper--title">
-                    <div class="texture__item">
+        <div class="blades__data">
+            <div class="blades__data-left">
+                <div class="blades__item-wrapper blades__item-wrapper--title">
+                    <div class="blades__item">
                         <p class="texture__item-p">Клинок Кофоед</p>
                     </div>
                 </div>
             </div>
-            <div class="texture__data-right">
-                <div class="texture__item-wrapper texture__item-wrapper--blade">
-                    <div class="texture__item">
-                        <img class="texture__picture" src="images/product_intro/blade/page-content/3.jpg" alt="">
-                        <p class="product-description__texture product-description__texture--blade">Скандинавский клинок пуукко</p>
-                        <p class="product-description__texture product-description__texture--blade">Модель: Кофоед - дизайн: Ким Кофоед (англ. Kim Kofoed)</p>
-                        <p class="product-description__texture product-description__texture--blade">Длина: 100мм (от пяты - по обух - до острия)</p>
-                        
-
-                        <p class="product-description__texture product-description__texture--blade">Стоимоть: {{ Illuminate\Support\Facades\DB::table('products')->where('product_blade_model', 'кофоед')->value('product_price')}} руб.</p>
+            <div class="blades__data-right">
+                <div class="blades__item-wrapper blades__item-wrapper--blade">
+                    <div class="blades__item">
+                        <div class="blades__picture-wrapper">
+                            <img class="blades__picture" src="images/product_intro/blade/page-content/3.jpg" alt="Скандинавский клинок пуукко - Модель: Кофоед">
+                        </div>
+                        <div class="blades__description-wrapper">
+                            <p class="blades__description">Скандинавский клинок пуукко</p>
+                            <div class="blades__description-row">
+                                <p class="blades__description blades__description--title">Модель:</p>
+                                <p class="blades__description blades__description--value">Кофоед - дизайн: Ким Кофоед (англ. Kim Kofoed)</p>
+                            </div>
+                            <div class="blades__description-row">
+                                <p class="blades__description blades__description--title">Длина:</p>
+                                <p class="blades__description blades__description--value">100мм (от пяты - по обуху - до острия)</p>
+                            </div>
+                            <div class="blades__description-row">
+                                <p class="blades__description blades__description--title">Толщина:</p>
+                                <p class="blades__description blades__description--value">4мм</p>
+                            </div>
+                            <p class="blades__description blades__description--price">Стоимоть: {{ Illuminate\Support\Facades\DB::table('products')->where('product_blade_model', 'кофоед')->value('product_price')}} руб.</p>
+                        </div>
                     </div>
                 </div>
             </div> 
         </div>
 
-        <div class="texture__data">
-            <div class="texture__data-left">
-                <div class="texture__item-wrapper texture__item-wrapper--title">
-                    <div class="texture__item">
+        <div class="blades__data">
+            <div class="blades__data-left">
+                <div class="blades__item-wrapper blades__item-wrapper--title">
+                    <div class="blades__item">
                         <p class="texture__item-p">Клинок Астрап</p>
                     </div>
                 </div>
             </div>
-            <div class="texture__data-right">
-                <div class="texture__item-wrapper texture__item-wrapper--blade">
-                    <div class="texture__item">
-                        <img class="texture__picture" src="images/product_intro/blade/page-content/4.jpg" alt="">
-                        <p class="product-description__texture product-description__texture--blade">Современный скандинавский клинок</p>
-                        <p class="product-description__texture product-description__texture--blade">Модель: Астрап - дизайн: Томми Астрап (англ. Tommy Astrup)</p>
-                        <p class="product-description__texture product-description__texture--blade">Длина: 85мм (от пяты - по обух - до острия)</p>
-                        
-
-                        <p class="product-description__texture product-description__texture--blade">Стоимоть: {{ Illuminate\Support\Facades\DB::table('products')->where('product_blade_model', 'астрап')->value('product_price')}} руб.</p>
+            <div class="blades__data-right">
+                <div class="blades__item-wrapper blades__item-wrapper--blade">
+                    <div class="blades__item">
+                        <div class="blades__picture-wrapper">
+                            <img class="blades__picture" src="images/product_intro/blade/page-content/4.jpg" alt="Современный скандинавский клинок - Модель: Астрап">
+                        </div>
+                        <div class="blades__description-wrapper">
+                            <p class="blades__description">Современный скандинавский клинок</p>
+                            <div class="blades__description-row">
+                                <p class="blades__description blades__description--title">Модель:</p>
+                                <p class="blades__description blades__description--value">Астрап - дизайн: Томми Астрап (англ. Tommy Astrup)</p>
+                            </div>
+                            <div class="blades__description-row">
+                                <p class="blades__description blades__description--title">Длина:</p>
+                                <p class="blades__description blades__description--value">85мм (от пяты - по обуху - до острия)</p>
+                            </div>
+                            <div class="blades__description-row">
+                                <p class="blades__description blades__description--title">Толщина:</p>
+                                <p class="blades__description blades__description--value">2,8мм</p>
+                            </div>
+                            <p class="blades__description blades__description--price">Стоимоть: {{ Illuminate\Support\Facades\DB::table('products')->where('product_blade_model', 'астрап')->value('product_price')}} руб.</p>
+                        </div>
                     </div>
                 </div>
             </div> 
         </div>
 
-
-        <div class="texture__data">
-            <div class="texture__data-left">
-                <div class="texture__item-wrapper texture__item-wrapper--title">
-                    <div class="texture__item">
+    
+        <div class="blades__data">
+            <div class="blades__data-left">
+                <div class="blades__item-wrapper blades__item-wrapper--title">
+                    <div class="blades__item">
                         <p class="texture__item-p">Клинок Роде</p>
                     </div>
                 </div>
             </div>
-            <div class="texture__data-right">
-                <div class="texture__item-wrapper texture__item-wrapper--blade">
-                    <div class="texture__item">
-                        <img class="texture__picture" src="images/product_intro/blade/page-content/5.jpg" alt="">
-                        <p class="product-description__texture product-description__texture--blade">Скандинавский клинок викингов</p>
-                        <p class="product-description__texture product-description__texture--blade">Модель: Роде - дизайн: Хеннинг Роде (англ. Henning Rohde)</p>
-                        <p class="product-description__texture product-description__texture--blade">Длина: 105мм (от пяты - по обух - до острия)</p>
-                        
-
-                        <p class="product-description__texture product-description__texture--blade">Стоимоть: {{ Illuminate\Support\Facades\DB::table('products')->where('product_blade_model', 'роде')->value('product_price')}} руб.</p>
+            <div class="blades__data-right">
+                <div class="blades__item-wrapper blades__item-wrapper--blade">
+                    <div class="blades__item">
+                        <div class="blades__picture-wrapper">
+                            <img class="blades__picture" src="images/product_intro/blade/page-content/5.jpg" alt="Скандинавский клинок викингов - Модель: Роде">
+                        </div>
+                        <div class="blades__description-wrapper">
+                            <p class="blades__description">Скандинавский клинок викингов</p>
+                            <div class="blades__description-row">
+                                <p class="blades__description blades__description--title">Модель:</p>
+                                <p class="blades__description blades__description--value">Роде - дизайн: Хеннинг Роде (англ. Henning Rohde)</p>
+                            </div>
+                            <div class="blades__description-row">
+                                <p class="blades__description blades__description--title">Длина:</p>
+                                <p class="blades__description blades__description--value">105мм (от пяты - по обуху - до острия)</p>
+                            </div>
+                            <div class="blades__description-row">
+                                <p class="blades__description blades__description--title">Толщина:</p>
+                                <p class="blades__description blades__description--value">4мм</p>
+                            </div>
+                            <p class="blades__description blades__description--price">Стоимоть: {{ Illuminate\Support\Facades\DB::table('products')->where('product_blade_model', 'роде')->value('product_price')}} руб.</p>
+                        </div>
                     </div>
                 </div>
             </div> 
@@ -176,47 +206,51 @@
 </section>
 
 <div class="gallery">
+
     <div class="gallery-item">
-        <a data-fslightbox="first-lightbox" href="/images/gallery/mammoth/molar-1.jpg" alt="фотография Зуб мамонта - синий цвет">
+        <a data-fslightbox="first-lightbox" href="/images/gallery/blades/blade-1.jpg" alt="фотография Зуб мамонта - синий цвет">
             <div class="gallery-item-content">
-                <img class="gallery-item-image" src="/images/gallery//mammoth/molar-1_thumb.jpg" alt="фотография Зуб мамонта - синий цвет превью">
+                <img class="gallery-item-image" src="/images/gallery//blades/blade-1_thumb.jpg" alt="фотография Зуб мамонта - синий цвет превью">
             </div>
             <div class="gallery-item-description">
-                <p class="gallery-item-description-text">Нож Станислава Стругунова</p>
-                <p class="gallery-item-description-text">материал - Зуб мамонта - синий цвет</p>
+                <p class="gallery-item-description-text">Нож Дженса Миккельсена</p>
+                <p class="gallery-item-description-text">клинок - Миккельсен</p>
             </div>
         </a>
     </div>
+   
     <div class="gallery-item">
-        <a data-fslightbox="first-lightbox" href="/images/gallery/mammoth/molar-2.jpg" alt="фотография Зуб мамонта - фиолетовый цвет">
+        <a data-fslightbox="first-lightbox" href="/images/gallery/blades/blade-2.jpg" alt="фотография Зуб мамонта - синий цвет">
             <div class="gallery-item-content">
-                <img class="gallery-item-image" src="/images/gallery//mammoth/molar-2_thumb.jpg" alt="фотография Зуб мамонта - фиолетовый цвет превью">
+                <img class="gallery-item-image" src="/images/gallery//blades/blade-2_thumb.jpg" alt="фотография Зуб мамонта - синий цвет превью">
             </div>
             <div class="gallery-item-description">
-                <p class="gallery-item-description-text">Нож Станислава Стругунова</p>
-                <p class="gallery-item-description-text">материал - Зуб мамонта - фиолетовый цвет</p>
+                <p class="gallery-item-description-text">Нож Дженса Миккельсена (2)</p>
+                <p class="gallery-item-description-text">клинок - Миккельсен</p>
             </div>
         </a>
     </div>
+    
     <div class="gallery-item">
-        <a data-fslightbox="first-lightbox" href="/images/gallery/mammoth/molar-3.jpg" alt="фотография Зуб мамонта - синий цвет">
+        <a data-fslightbox="first-lightbox" href="/images/gallery/blades/blade-3.jpg" alt="фотография Зуб мамонта - синий цвет">
             <div class="gallery-item-content">
-                <img class="gallery-item-image" src="/images/gallery//mammoth/molar-3_thumb.jpg" alt="фотография Зуб мамонта - синий цвет превью">
+                <img class="gallery-item-image" src="/images/gallery//blades/blade-3_thumb.jpg" alt="фотография Зуб мамонта - синий цвет превью">
             </div>
             <div class="gallery-item-description">
-                <p class="gallery-item-description-text">Нож Станислава Стругунова</p>
-                <p class="gallery-item-description-text">материал - Зуб мамонта - синий цвет</p>
+                <p class="gallery-item-description-text">Нож Томми Астрапа</p>
+                <p class="gallery-item-description-text">клинок - Астрап</p>
             </div>
         </a>
     </div>
+
     <div class="gallery-item">
-        <a data-fslightbox="first-lightbox" href="/images/gallery/mammoth/molar-4.jpg" alt="фотография Зуб мамонта - зеленый цвет">
+        <a data-fslightbox="first-lightbox" href="/images/gallery/blades/blade-4.jpg" alt="фотография Зуб мамонта - синий цвет">
             <div class="gallery-item-content">
-                <img class="gallery-item-image" src="/images/gallery//mammoth/molar-4_thumb.jpg" alt="фотография Зуб мамонта - зеленый цвет превью">
+                <img class="gallery-item-image" src="/images/gallery//blades/blade-4_thumb.jpg" alt="фотография Зуб мамонта - синий цвет превью">
             </div>
             <div class="gallery-item-description">
-                <p class="gallery-item-description-text">Нож Станислава Стругунова</p>
-                <p class="gallery-item-description-text">материал - Зуб мамонта - зеленый цвет</p>
+                <p class="gallery-item-description-text">Нож Томми Астрапа (2)</p>
+                <p class="gallery-item-description-text">клинок - Астрап</p>
             </div>
         </a>
     </div>
---
+++ resources/views/pages/blades-page/includes/blades-3-table.blade.php
@@ -0,0 +1,107 @@
+
+<section class="title" id="">
+    <div class="title__wrapper">
+        <p class="title__h2">Таблица</p>
+        <p class="title__p">
+            Сравнительная таблица охотничих клинков Раффир.  
+        </p>
+    </div>
+</section>
+
+<section class="blades">
+
+    <div class="texture__title">
+        <p class="texture__title-h3">
+            Клинки уже готовые к монтажу.
+        </p>
+    </div>
+
+
+    <div class="table">
+
+        <table class="table__data">
+            <thead class="table__thead">
+                <tr class="table__tr">
+                    <th class="table__td">Название</th>
+                    <th class="table__td" scope="colgroup">Размеры ( д-ш-т )<sup>1</sup>, мм.</th>
+                    <th class="table__td" scope="colgroup">Вес, гр.</th>
+                    <th class="table__td" scope="colgroup">Твердость ( HRC )</th>
+                    <th class="table__td" scope="colgroup">Сталь<sup>2</sup></th>
+                </tr>
+            </thead>
+    
+            <tbody>
+                <tr class="table__tr">
+                    <td class="table__td">Рабек</td>
+                    <td class="table__td">75 x 22 x 2,8</td>
+                    <td class="table__td">26</td>
+                    <td class="table__td">59 - 60</td>
+                    <td class="table__td">440С/N690, 110 слоев</td>
+                </tr>
+                <tr class="table__tr">
+                    <td class="table__td">Миккельсен</td>
+                    <td class="table__td">85 x 27 x 2,8</td>
+                    <td class="table__td">41</td>
+                    <td class="table__td">59 - 60</td>
+                    <td class="table__td">440С/N690, 110 слоев </td>
+                </tr>
+                <tr class="table__tr">
+                    <td class="table__td">Кофоед</td>
+                    <td class="table__td">100 x 22,5 x 4</td>
+                    <td class="table__td">45</td>
+                    <td class="table__td">59 - 60</td>
+                    <td class="table__td">440С/N690, 110 слоев </td>
+                </tr>
+                <tr class="table__tr">
+                    <td class="table__td">Астрап</td>
+                    <td class="table__td">85 x 26 x 2,8</td>
+                    <td class="table__td">39</td>
+                    <td class="table__td">59 - 60</td>
+                    <td class="table__td">440С/N690, 110 слоев </td>
+                </tr>
+                <tr class="table__tr">
+                    <td class="table__td">Роде<sup>3</sup></td>
+                    <td class="table__td">105 x 33,5 x 4</td>
+                    <td class="table__td">48</td>
+                    <td class="table__td">59 - 60</td>
+                    <td class="table__td">440С/N690, 110 слоев </td>
+                </tr>
+            </tbody>
+
+        </table>
+
+    </div>
+
+    <div class="table__description">
+        <p class="table__description-text"><sup>1 </sup> Длина клинка измеряется от пяты - по обуху - до острия.
+        </p>
+        <p class="table__description-text"><sup>2 </sup> В клинках мы используем 440С/N690 высоколегированную сталь, клинок - нержавеющий дамаск 110 слоев, протравлен и отполирован.
+        </p>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+

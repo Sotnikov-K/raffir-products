ac929c3d3e03d47ae37953a0970c24c361cf96e8
Kirill Sotnikov <sotnikovmail@yandex.ru>
2023-03-17 23:14:07 +1000

    knive page - frontend - knive section 2

--- resources/views/pages/knives-page/includes/knives-2-knives.blade.php
+++ resources/views/pages/knives-page/includes/knives-2-knives.blade.php
@@ -8,5 +8,128 @@
 </section>
 
 <section class="knives">
-
 </section>
+
+<div class="gallery">
+
+    <div class="gallery-item gallery-item--knives">
+        <a data-fslightbox="first-lightbox" href="/images/knives/1-standart.jpg" alt="">
+            <div class="gallery-item-content">
+                <img class="gallery-item-image" src="/images/knives/1-thumb.jpg" alt="">
+            </div>
+            <div class="gallery-item-description">
+                <p class="gallery-item-description-text">Нож Станислава Стругунова</p>
+                <p class="gallery-item-description-text">Кап тополя - синий цвет</p>
+            </div>
+        </a>
+    </div>
+   
+    <div class="gallery-item gallery-item--knives">
+        <a data-fslightbox="first-lightbox" href="/images/knives/2-standart.jpg" alt="Нож Станислава Стругунова">
+            <div class="gallery-item-content">
+                <img class="gallery-item-image" src="/images/knives/2-thumb.jpg" alt="">
+            </div>
+            <div class="gallery-item-description">
+                <p class="gallery-item-description-text">Нож Станислава Стругунова</p>
+                <p class="gallery-item-description-text">Композит волна - алюм зеленый цвет</p>
+            </div>
+        </a>
+    </div>
+    
+    <div class="gallery-item gallery-item--knives">
+        <a data-fslightbox="first-lightbox" href="/images/knives/3-standart.jpg" alt="">
+            <div class="gallery-item-content">
+                <img class="gallery-item-image" src="/images/knives/3-thumb.jpg" alt="">
+            </div>
+            <div class="gallery-item-description">
+                <p class="gallery-item-description-text">Нож Станислава Стругунова</p>
+                <p class="gallery-item-description-text">Композит волна - нобл желтая бронза</p>
+            </div>
+        </a>
+    </div>
+
+    <div class="gallery-item gallery-item--knives">
+        <a data-fslightbox="first-lightbox" href="/images/knives/4-standart.jpg" alt="">
+            <div class="gallery-item-content">
+                <img class="gallery-item-image" src="/images/knives/4-thumb.jpg" alt="">
+            </div>
+            <div class="gallery-item-description">
+                <p class="gallery-item-description-text">Нож компании Береза</p>
+                <p class="gallery-item-description-text">Композит васп - алюм синий цвет</p>
+            </div>
+        </a>
+    </div>
+
+    <div class="gallery-item gallery-item--knives">
+        <a data-fslightbox="first-lightbox" href="/images/knives/5-standart.jpg" alt="">
+            <div class="gallery-item-content">
+                <img class="gallery-item-image" src="/images/knives/5-thumb.jpg" alt="">
+            </div>
+            <div class="gallery-item-description">
+                <p class="gallery-item-description-text">Нож компании Береза</p>
+                <p class="gallery-item-description-text">Композит волна - нобл латунь</p>
+            </div>
+        </a>
+    </div>
+
+    <div class="gallery-item gallery-item--knives">
+        <a data-fslightbox="first-lightbox" href="/images/knives/6-standart.jpg" alt="">
+            <div class="gallery-item-content">
+                <img class="gallery-item-image" src="/images/knives/6-thumb.jpg" alt="">
+            </div>
+            <div class="gallery-item-description">
+                <p class="gallery-item-description-text">Нож Алексей Куликова</p>
+                <p class="gallery-item-description-text">Композит волна - SFX синий уран</p>
+            </div>
+        </a>
+    </div>
+
+    <div class="gallery-item gallery-item--knives">
+        <a data-fslightbox="first-lightbox" href="/images/knives/7-standart.jpg" alt="">
+            <div class="gallery-item-content">
+                <img class="gallery-item-image" src="/images/knives/7-thumb.jpg" alt="">
+            </div>
+            <div class="gallery-item-description">
+                <p class="gallery-item-description-text">Нож Алексей Куликова</p>
+                <p class="gallery-item-description-text">Композит волна - SFX синий уран (в темноте)</p>
+            </div>
+        </a>
+    </div>
+
+    <div class="gallery-item gallery-item--knives">
+        <a data-fslightbox="first-lightbox" href="/images/knives/8-standart.jpg" alt="">
+            <div class="gallery-item-content">
+                <img class="gallery-item-image" src="/images/knives/8-thumb.jpg" alt="">
+            </div>
+            <div class="gallery-item-description">
+                <p class="gallery-item-description-text">Нож от Barada knife</p>
+                <p class="gallery-item-description-text">Шпальт бука - лимонный цвет</p>
+            </div>
+        </a>
+    </div>
+
+    <div class="gallery-item gallery-item--knives">
+        <a data-fslightbox="first-lightbox" href="/images/knives/9-standart.jpg" alt="">
+            <div class="gallery-item-content">
+                <img class="gallery-item-image" src="/images/knives/9-thumb.jpg" alt="">
+            </div>
+            <div class="gallery-item-description">
+                <p class="gallery-item-description-text">Нож Станислава Стругунова</p>
+                <p class="gallery-item-description-text">Шпальт бука - синий цвет</p>
+            </div>
+        </a>
+    </div>
+
+    <div class="gallery-item gallery-item--knives">
+        <a data-fslightbox="first-lightbox" href="/images/knives/10-standart.jpg" alt="">
+            <div class="gallery-item-content">
+                <img class="gallery-item-image" src="/images/knives/10-thumb.jpg" alt="">
+            </div>
+            <div class="gallery-item-description">
+                <p class="gallery-item-description-text">Нож Станислава Стругунова</p>
+                <p class="gallery-item-description-text">Кап тополя - зеленый цвет</p>
+            </div>
+        </a>
+    </div>
+
+</div>
